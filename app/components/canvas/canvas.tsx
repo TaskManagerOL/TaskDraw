a701641b52bab9ce8792aa1f5ae1bfb735088b32
TaskManagerOL <1975771886@qq.com>
2025-08-11 10:24:57 +0800

    refactor:canvas结构分层

--- app/components/canvas/canvas.tsx
+++ app/components/canvas/canvas.tsx
@@ -1,61 +1,24 @@
 'use client'
-import { useRef, useEffect, useState } from 'react';
-
 import ToolBar from "../toolBar/toolBar";
 import InfoBar from "../infoBar/infoBar";
 
-import { useMouseHandlers } from '../../hook/useMouseHandlers';
-import { redrawCanvas } from "../../utils/draw";
+import useCanvas from "../../hook/useCanvas";
 
 export default function Canvas() {
-  const canvasRef = useRef(null);
-  const [viewport, setViewport] = useState({ x: 0, y: 0 });
-  const [scale, setScale] = useState(1);
-  const [isPanning, setIsPanning] = useState(false);
-  const [panStart, setPanStart] = useState({ x: 0, y: 0 });
-  const [tool, setTool] = useState('');
-  const [color, setColor] = useState('#000000');
-  const [lineWidth, setLineWidth] = useState(3);
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [elements, setElements] = useState([]);
-  const [tempElement, setTempElement] = useState(null);
-
-  const mouseHandlers = useMouseHandlers(
-    { tool, color, lineWidth, isDrawing, canvasRef, scale,viewport,isPanning,panStart,tempElement,elements },
-    { setElements, setIsDrawing, setTempElement,setIsPanning,setPanStart,setViewport,setScale }
-  );
-
-    // 初始化Canvas
-  useEffect(() => {
-    const updateCanvasSize = () => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
-        const currentCenterX = canvas.width / (2 * scale) + viewport.x;
-        const currentCenterY = canvas.height / (2 * scale) + viewport.y;
-        const newWidth = window.innerWidth;
-        const newHeight = window.innerHeight;
-        canvas.width = newWidth;
-        canvas.height = newHeight;
-        const newViewportX = currentCenterX - newWidth / (2 * scale);
-        const newViewportY = currentCenterY - newHeight / (2 * scale);
-        setViewport({
-            x: newViewportX,
-            y: newViewportY
-        });
-        redrawCanvas(canvasRef,elements,tempElement,viewport,scale,color,lineWidth,setElements);
-    };
-    updateCanvasSize();
-    window.addEventListener('resize', updateCanvasSize);
-    return () => {
-      window.removeEventListener('resize', updateCanvasSize);
-    };
-  }, []);
-
-  // 绘制工具变化时重绘
-  useEffect(() => {
-    redrawCanvas(canvasRef,elements,tempElement,viewport,scale,color,lineWidth);
-  }, [viewport, scale, elements, tempElement]);
-
+  const {
+    canvasRef,
+    mouseHandlers,
+    tool,
+    setTool,
+    color,
+    setColor,
+    lineWidth,
+    setLineWidth,
+    viewport,
+    scale,
+    firstTool,
+    setFirstTool
+  } = useCanvas();
   return(
     <div className="relative w-screen h-screen overflow-hidden">
       <canvas
@@ -75,6 +38,8 @@ export default function Canvas() {
         setColor={setColor}
         lineWidth={lineWidth} 
         setLineWidth={setLineWidth}
+        firstTool={firstTool}
+        setFirstTool={setLineWidth}
       />
       <InfoBar
         tool={tool}
---
+++ app/hook/useCanvas.ts
@@ -0,0 +1,71 @@
+'use client'
+import { useRef, useEffect, useState } from 'react';
+
+import { useMouseHandlers } from './useMouseHandlers';
+import { redrawCanvas } from "../utils/draw";
+
+export default function useCanvas() {
+  const canvasRef = useRef(null);
+  const [viewport, setViewport] = useState({ x: 0, y: 0 });
+  const [scale, setScale] = useState(1);
+  const [isPanning, setIsPanning] = useState(false);
+  const [panStart, setPanStart] = useState({ x: 0, y: 0 });
+  const [tool, setTool] = useState('ballpointPen');
+  const [color, setColor] = useState('#000000');
+  const [lineWidth, setLineWidth] = useState(3);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [elements, setElements] = useState([]);
+  const [tempElement, setTempElement] = useState(null);
+  const [firstTool,setFirstTool] = useState('draw')
+
+  const mouseHandlers = useMouseHandlers(
+    { tool, color, lineWidth, isDrawing, canvasRef, scale,viewport,isPanning,panStart,tempElement,elements },
+    { setElements, setIsDrawing, setTempElement,setIsPanning,setPanStart,setViewport,setScale }
+  );
+
+    // 初始化Canvas
+  useEffect(() => {
+    const updateCanvasSize = () => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+        const currentCenterX = canvas.width / (2 * scale) + viewport.x;
+        const currentCenterY = canvas.height / (2 * scale) + viewport.y;
+        const newWidth = window.innerWidth;
+        const newHeight = window.innerHeight;
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+        const newViewportX = currentCenterX - newWidth / (2 * scale);
+        const newViewportY = currentCenterY - newHeight / (2 * scale);
+        setViewport({
+            x: newViewportX,
+            y: newViewportY
+        });
+        redrawCanvas(canvasRef,elements,tempElement,viewport,scale,color,lineWidth,setElements);
+    };
+    updateCanvasSize();
+    window.addEventListener('resize', updateCanvasSize);
+    return () => {
+      window.removeEventListener('resize', updateCanvasSize);
+    };
+  }, []);
+
+  // 绘制工具变化时重绘
+  useEffect(() => {
+    redrawCanvas(canvasRef,elements,tempElement,viewport,scale,color,lineWidth);
+  }, [viewport, scale, elements, tempElement]);
+
+  return { 
+    canvasRef,
+    mouseHandlers,
+    tool,
+    setTool,
+    color,
+    setColor,
+    lineWidth,
+    setLineWidth,
+    viewport,
+    scale,
+    firstTool,
+    setFirstTool
+  }
+}
